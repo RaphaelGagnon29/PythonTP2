3205df80d2587b3a69c59ebe6a2e54f3b0c75e0e
Raphael Gagnon <raphaelgagnon29@gmail.com>
2023-05-10 22:57:29 -0400

    impex.py tentative d'ajout de la méthode export_csv(utilisateur_json)

--- impex/impex.py
+++ impex/impex.py
@@ -1,6 +1,7 @@
 from client import client
 import csv
 from xml.dom import minidom
+import time
 
 
 class Utilisateur:
@@ -40,6 +41,17 @@ class Utilisateur:
                 }
             }
 
+    @staticmethod
+    def export_csv(utilisateur_json):
+        timestamp = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+        with open(f'./export_{timestamp}.csv', 'w', newline='') as csv_file:
+            champs = ['email', 'mdp', 'nom', 'prenom', 'type', 'adresse_no', 'adresse_rue', 'adresse_ville',
+                      'adresse_prov', 'adresse_pays', 'adresse_cp']
+            csv_dict_writer = csv.DictWriter(csv_file, fieldnames=champs)
+            csv_dict_writer.writeheader()
+            csv_dict_writer.writerow(utilisateur_json)
+
+
 class Chalet:
 
     def __init__(self, id, nom, url_image, geo_lat, geo_long):
@@ -65,7 +77,7 @@ class Chalet:
             }
 
 
-liste_utilisateurs = []
+liste_utilisateurs_json = []
 with open('./data/utilisateurs.csv', 'r') as csv_utilisateurs:
     csv_reader = csv.reader(csv_utilisateurs)
     for utilisateur in csv_reader:
@@ -75,21 +87,25 @@ with open('./data/utilisateurs.csv', 'r') as csv_utilisateurs:
             objet = Utilisateur(utilisateur[0], utilisateur[1], utilisateur[2], utilisateur[3], utilisateur[4],
                                 utilisateur[5], utilisateur[6], utilisateur[7], utilisateur[8], utilisateur[9],
                                 utilisateur[10])
-            liste_utilisateurs.append(objet.csv_en_json())
+            liste_utilisateurs_json.append(objet.csv_en_json())
 
-for i in liste_utilisateurs:
+for i in liste_utilisateurs_json:
     client.ClientServeurChalet('http://localhost:8000').ajout_utilisateur(i)
 
 
-liste_chalets = []
-with open('./data/ chalets,csv', 'r') as csv_chalets:
+liste_chalets_json = []
+with open('./data/chalets.csv', 'r') as csv_chalets:
     csv_reader_ = csv.reader(csv_chalets)
     for chalet in csv_reader_:
         if chalet[0] == 'id':
             continue
         else:
             objet_ = Chalet(chalet[0], chalet[1], chalet[2], chalet[3], chalet[4])
-            liste_chalets.append(objet_.csv_en_json())
+            liste_chalets_json.append(objet_.csv_en_json())
 
-for j in liste_chalets:
+for j in liste_chalets_json:
     client.ClientServeurChalet('http://localhost:8000').ajout_chalet(j)
+
+
+for x in liste_utilisateurs_json:
+    Utilisateur.export_csv(x)
